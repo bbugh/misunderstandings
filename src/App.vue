5b11be7b4a7d86074f51ae5db21c72624fdb62d2
Brian Bugh <438465+bbugh@users.noreply.github.com>
2026-01-11 01:35:04 +0800

    Add querystring and more image formats while I was at it (#6)

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 import { imageUrl } from '@/utils'
 
-const sizes = ['small', 'big'] as const
 const subjects = ['i', 'you', 'he', 'she', 'they'] as const
 
-type Size = (typeof sizes)[number]
+type Size = 'a-little' | 'a-lot'
 type Subject = (typeof subjects)[number]
 
 const leftBookSubject = ref<Subject>('you')
-const leftBookSize = ref<Size>('small')
-
+const leftBookSize = ref<Size>('a-little')
 const rightBookSubject = ref<Subject>('i')
-const rightBookSize = ref<Size>('big')
+const rightBookSize = ref<Size>('a-lot')
+
+const variation = new URLSearchParams(window.location.search)
+  .get('v')
+  ?.match(/(\w+)-said-(a-little|a-lot)-(\w+)-read-(a-little|a-lot)/)
+
+if (variation) {
+  leftBookSubject.value = variation[1] as Subject
+  leftBookSize.value = variation[2] as Size
+  rightBookSubject.value = variation[3] as Subject
+  rightBookSize.value = variation[4] as Size
+}
 
 const bookId = computed(
   () =>
     `meme-${leftBookSubject.value}-said-${leftBookSize.value}-${rightBookSubject.value}-read-${rightBookSize.value}`
 )
+
+watch([leftBookSubject, leftBookSize, rightBookSubject, rightBookSize], () => {
+  const meme = `${leftBookSubject.value}-said-${leftBookSize.value}-${rightBookSubject.value}-read-${rightBookSize.value}`
+  const newUrl = `${window.location.pathname}?v=${meme}`
+  window.history.replaceState(null, '', newUrl)
+})
 
 const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
 </script>
@@ -43,8 +58,8 @@ const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
         said
 
         <select v-model="leftBookSize" class="pl-1">
-          <option value="small">a little</option>
-          <option value="big">a lot</option>
+          <option value="a-little">a little</option>
+          <option value="a-lot">a lot</option>
         </select>
 
         and
@@ -58,18 +73,22 @@ const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
         read a
 
         <select v-model="rightBookSize" class="pl-1">
-          <option value="small">little</option>
-          <option value="big">lot</option>
+          <option value="a-little">little</option>
+          <option value="a-lot">lot</option>
         </select>
 
         into it
       </p>
 
-      <img
-        :src="imageUrl(`${bookId}.png`)"
-        :alt="bookId"
-        class="p-4 border border-gray-300 shadow-lg max-w-96"
-      />
+      <picture>
+        <source :srcset="imageUrl(`${bookId}.avif`)" type="image/avif" />
+        <source :srcset="imageUrl(`${bookId}.webp`)" type="image/webp" />
+        <img
+          :src="imageUrl(`${bookId}.png`)"
+          :alt="bookId"
+          class="p-4 border border-gray-300 shadow-lg max-w-96"
+        />
+      </picture>
 
       <div class="text-xs italic text-center text-gray-400">
         <p>Right click on image or tap to hold to save.</p>
